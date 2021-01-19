0db880e22c2d7ab93743afc9cc2136917381f488
Jesús Corvo <44329031+Mastercorvo@users.noreply.github.com>
2021-01-18 21:18:30 -0400

    Puliendo el nuevo motor

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,47 @@
 
 import './App.css';
 
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
+
+import {useImage} from 'react-image';
+
+import Zone1 from "./zona 1/Zone1";
+
+const SelenaHabla = 'https://i.ibb.co/rGM9kQR/Selena-Stickers1.png';
+const SelenaHablaSeria = 'https://i.ibb.co/9tVZNgx/Selena-Stickers2.png';
+const SelenaHablaTriste = 'https://i.ibb.co/P1k9ph8/Selena-Stickers5.png';
+const SelenaDesconfia = 'https://i.ibb.co/5R6tPPQ/Selena-Stickers3.png';
+const SelenaAvergonzada = 'https://i.ibb.co/L0TWpy5/Selena-Stickers10.png';
+
+const Selenas ={SelenaHabla, SelenaHablaSeria, SelenaHablaTriste, SelenaDesconfia, SelenaAvergonzada}
 
 function App() {
+
+  useImage({srcList:[SelenaHabla, SelenaHablaSeria, SelenaHablaTriste, SelenaDesconfia, SelenaAvergonzada]});
 
   const arrayTexts = useRef([]);
 
   const [TEXT, setTEXT] = useState('');
   const writing = useRef(false);
+  const currentText = useRef('');
+  const forceText = useRef(false);
+  const anyText = useRef(false);
+  const [avatar, setAvatar] = useState('');
+  const [showText, setShowText] = useState(false)
 
-  function escribir(DATA) {
+  const [newFoto, setNewFoto] = useState(false);
+
+  function escribir(_DATA) {
+
+    const DATA = _DATA?_DATA:{text:['','']};
 
     let { text, speed, wait, img, replace } = DATA;
 
+    setAvatar(()=>Selenas[img])
+
     if(speed === 'insta'){
       
-      setTEXT(()=>text.join``)
+      setTEXT(()=>text.join``);
       
       return false;
       
@@ -25,7 +50,7 @@ function App() {
     writing.current = true;
     
     if(replace === undefined) replace = true;
-    if(speed === undefined) speed = 30;
+    if(speed === undefined) speed = 40;
     if(wait === undefined) wait = 0;
 
     const textOriginal = text[0];
@@ -35,6 +60,8 @@ function App() {
     if(replace) setTEXT(()=>'');
     
     setTEXT(v=> v+text[0]);
+
+    currentText.current = text.join``;
 
     for(let i = 0; i <= wait/100; i++){
 
@@ -68,11 +95,12 @@ function App() {
   
         setTimeout(()=>{
   
-          setTEXT(v=>v + text[1][i]); 
+          if(!forceText.current) setTEXT(v=>v + text[1][i]); 
   
           if(i === (text[1].length - 1)){
   
             writing.current = false;
+            if(arrayTexts.current.length === 0) anyText.current = false
   
           }
   
@@ -84,32 +112,78 @@ function App() {
 
   }
 
-  function inputText(data){
+  function inputText(data, purgue){
+
+    if(purgue) arrayTexts.current = [];
+
+    if(anyText.current) return false;
 
     arrayTexts.current.push(data);
     
   }
 
-  function outputText(purgue){
+  const forceTextCount = useRef(0);
 
+  function outputText(power){
+
+    if(anyText.current && !power) return false;
+
+    setNewFoto(()=>true)
+    setShowText(()=>true)
+
+    anyText.current = true;
+
+    forceText.current = false;
+
+    if(writing.current){
+
+      setNewFoto(()=>false);
+
+      forceText.current = true;
+
+      setTEXT(()=>currentText.current);
+
+      if(arrayTexts.current.length === 0) anyText.current = false;
+
+      if(forceTextCount.current === 1) setShowText(false);
+      
+      if(arrayTexts.current.length === 0) forceTextCount.current = 1;
+
+      return false;
+
+    }
+
+    if(arrayTexts.current.length === 0) setShowText(false);
+    
     escribir(arrayTexts.current.shift());
     
   }
 
+  useEffect(()=>{
+
+    inputText({text:['— Selena: ','Haa... ¿Dónde estoy?'], img: 'SelenaHablaTriste'});
+    inputText({text:['— Selena: ','No sé como llegue aquí pero debo salir rapido.'], img: 'SelenaHablaSeria'});
+    outputText();
+
+  }, [])
+
   function textHandler(){
 
-    inputText({text:['hola','amigos']});
-    inputText({text:['hola','amigos']});
-    outputText();
+    outputText(true);
     
   }
 
   return (
-    <div className="App">
 
-      <div className="texto" onClick={textHandler}>{TEXT}</div>
+      <div className="App">
+        <Zone1 inputText={inputText} outputText={outputText}/>
+        <div className="texto" onClick={textHandler} style={{display:showText?'block':'none'}}>
+        {TEXT}
+        <div className={'foto ' + (newFoto?'boom':'')} onAnimationEnd={()=>{setNewFoto(()=>false)}} style={{backgroundImage: `url(${avatar})`} }></div>
+        </div>
 
-    </div>
+      </div>
+
   );
 }
 
---
+++ src/zona 1/Zone1.jsx
@@ -0,0 +1,390 @@
+
+import './zone1.css';
+
+function Zone1({inputText, outputText}) {
+
+    function cajasHandler() {
+
+        inputText({text:['— Selena: ','Haa... ¿Dónde estoy?'], img: SelenaHablaTriste});
+        
+    }
+
+    return (<div className="Zone1">
+
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      id="svg8"
+      width="1920"
+      height="1080"
+      version="1.1"
+      viewBox="0 0 508 285.75"
+      className="main-svg"
+    >
+      <defs id="defs2">
+        <clipPath id="clipPath878" clipPathUnits="userSpaceOnUse">
+          <path
+            id="rect880"
+            fill="#000"
+            fillOpacity="1"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="5.292"
+            d="M297.089 68.792H408.97V180.673H297.089z"
+          ></path>
+        </clipPath>
+        <clipPath id="clipPath878-1" clipPathUnits="userSpaceOnUse">
+          <path
+            id="rect880-7"
+            fill="#000"
+            fillOpacity="1"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="5.292"
+            d="M297.089 68.792H408.97V180.673H297.089z"
+          ></path>
+        </clipPath>
+        <clipPath id="clipPath878-1-6" clipPathUnits="userSpaceOnUse">
+          <path
+            id="rect880-7-8"
+            fill="#000"
+            fillOpacity="1"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="5.292"
+            d="M297.089 68.792H408.97V180.673H297.089z"
+          ></path>
+        </clipPath>
+        <clipPath id="clipPath878-1-6-2" clipPathUnits="userSpaceOnUse">
+          <path
+            id="rect880-7-8-1"
+            fill="#000"
+            fillOpacity="1"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="5.292"
+            d="M297.089 68.792H408.97V180.673H297.089z"
+          ></path>
+        </clipPath>
+        <clipPath id="clipPath878-1-6-2-6" clipPathUnits="userSpaceOnUse">
+          <path
+            id="rect880-7-8-1-8"
+            fill="#000"
+            fillOpacity="1"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="5.292"
+            d="M297.089 68.792H408.97V180.673H297.089z"
+          ></path>
+        </clipPath>
+      </defs>
+      <g id="layer1">
+        <path
+          id="rect833"
+          fill="#0073ff"
+          fillOpacity="1"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="3.496"
+          d="M0 161.018H508V285.75H0z"
+        ></path>
+        <path
+          id="rect835"
+          fill="#00abff"
+          fillOpacity="1"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="5.292"
+          d="M0 0H508V161.018H0z"
+        ></path>
+        <g id="caja-electrica" fillOpacity="1" stroke="none" strokeOpacity="1">
+          <path
+            id="rect1041"
+            fill="#6e6e6e"
+            strokeDasharray="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeMiterlimit="4"
+            strokeWidth="14.965"
+            d="M57.452 49.893H120.196V116.795H57.452z"
+          ></path>
+          <path
+            id="rect1043"
+            fill="#6e6e6e"
+            strokeDasharray="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeMiterlimit="4"
+            strokeWidth="8.025"
+            d="M83.722 0H93.92699999999999V49.893H83.722z"
+          ></path>
+          <path
+            id="path1045"
+            fill="#fff400"
+            strokeLinecap="butt"
+            strokeLinejoin="miter"
+            strokeWidth="0.069"
+            d="M85.108 79.717c-.523.345-7.9 13.736-7.9 13.736L90.63 88.91 78.26 106.197l21.887-9.268-8.775-.02 14.36-16.83-16.003 3.05 6.358-21.085z"
+          ></path>
+        </g>
+        <g
+          id="cajas"
+          onClick={cajasHandler}
+          fillOpacity="1"
+          stroke="#c67700"
+          strokeDasharray="none"
+          strokeLinecap="round"
+          strokeMiterlimit="4"
+          strokeOpacity="1"
+          strokeWidth="13.229"
+        >
+          <g id="g926" transform="matrix(.58912 0 0 .58912 176.79 175.826)">
+            <path
+              id="rect839"
+              fill="#945900"
+              strokeLinejoin="round"
+              d="M309.94 -143.631H421.821V-31.75H309.94z"
+            ></path>
+            <g
+              id="g860-9"
+              fill="none"
+              strokeLinejoin="miter"
+              clipPath="url(#clipPath878)"
+              transform="translate(12.851 -212.423)"
+            >
+              <path id="path841-5" d="M298.412 70.115L407.647 179.35"></path>
+              <path id="path841-3-1" d="M407.647 70.115L298.412 179.35"></path>
+            </g>
+          </g>
+          <g id="g926-6" transform="matrix(.2991 0 0 .2991 227.344 168.536)">
+            <path
+              id="rect839-9"
+              fill="#945900"
+              strokeLinejoin="round"
+              d="M309.94 -143.631H421.821V-31.75H309.94z"
+            ></path>
+            <g
+              id="g860-9-5"
+              fill="none"
+              strokeLinejoin="miter"
+              clipPath="url(#clipPath878-1)"
+              transform="translate(12.851 -212.423)"
+            >
+              <path id="path841-5-8" d="M298.412 70.115L407.647 179.35"></path>
+              <path
+                id="path841-3-1-6"
+                d="M407.647 70.115L298.412 179.35"
+              ></path>
+            </g>
+          </g>
+          <g id="g926-6-7" transform="matrix(.2991 0 0 .2991 282.907 94.83)">
+            <path
+              id="rect839-9-2"
+              fill="#945900"
+              strokeLinejoin="round"
+              d="M309.94 -143.631H421.821V-31.75H309.94z"
+            ></path>
+            <g
+              id="g860-9-5-8"
+              fill="none"
+              strokeLinejoin="miter"
+              clipPath="url(#clipPath878-1-6)"
+              transform="translate(12.851 -212.423)"
+            >
+              <path
+                id="path841-5-8-7"
+                d="M298.412 70.115L407.647 179.35"
+              ></path>
+              <path
+                id="path841-3-1-6-8"
+                d="M407.647 70.115L298.412 179.35"
+              ></path>
+            </g>
+          </g>
+          <g
+            id="g926-6-7-8"
+            transform="matrix(.16664 0 0 .16664 377.865 164.427)"
+          >
+            <path
+              id="rect839-9-2-6"
+              fill="#945900"
+              strokeLinejoin="round"
+              d="M309.94 -143.631H421.821V-31.75H309.94z"
+            ></path>
+            <g
+              id="g860-9-5-8-7"
+              fill="none"
+              strokeLinejoin="miter"
+              clipPath="url(#clipPath878-1-6-2)"
+              transform="translate(12.851 -212.423)"
+            >
+              <path
+                id="path841-5-8-7-7"
+                d="M298.412 70.115L407.647 179.35"
+              ></path>
+              <path
+                id="path841-3-1-6-8-9"
+                d="M407.647 70.115L298.412 179.35"
+              ></path>
+            </g>
+          </g>
+          <g
+            id="g926-6-7-8-5"
+            transform="matrix(.16664 0 0 .16664 377.865 143.578)"
+          >
+            <path
+              id="rect839-9-2-6-8"
+              fill="#945900"
+              strokeLinejoin="round"
+              d="M309.94 -143.631H421.821V-31.75H309.94z"
+            ></path>
+            <g
+              id="g860-9-5-8-7-3"
+              fill="none"
+              strokeLinejoin="miter"
+              clipPath="url(#clipPath878-1-6-2-6)"
+              transform="translate(12.851 -212.423)"
+            >
+              <path
+                id="path841-5-8-7-7-1"
+                d="M298.412 70.115L407.647 179.35"
+              ></path>
+              <path
+                id="path841-3-1-6-8-9-1"
+                d="M407.647 70.115L298.412 179.35"
+              ></path>
+            </g>
+          </g>
+        </g>
+        <g
+          id="pantalla"
+          strokeDasharray="none"
+          strokeLinecap="round"
+          strokeMiterlimit="4"
+          strokeOpacity="1"
+        >
+          <rect
+            id="rect1080"
+            width="84.131"
+            height="58.308"
+            x="209.911"
+            y="21.167"
+            fill="#000"
+            fillOpacity="1"
+            stroke="none"
+            strokeLinejoin="round"
+            strokeWidth="13.229"
+            ry="7.938"
+          ></rect>
+          <circle
+            id="path1082"
+            cx="224.518"
+            cy="34.774"
+            r="7.938"
+            fill="#fff"
+            fillOpacity="1"
+            stroke="none"
+            strokeLinejoin="round"
+            strokeWidth="13.229"
+          ></circle>
+          <g
+            id="g1144"
+            fill="#2bff00"
+            fillOpacity="1"
+            stroke="none"
+            strokeLinejoin="round"
+            strokeWidth="13.229"
+            transform="translate(0 3.742)"
+          >
+            <rect
+              id="rect1086"
+              width="9.827"
+              height="9.827"
+              x="244.74"
+              y="57.641"
+              rx="0"
+              ry="0"
+            ></rect>
+            <rect
+              id="rect1086-7"
+              width="9.827"
+              height="9.827"
+              x="257.969"
+              y="57.641"
+              rx="0"
+              ry="0"
+            ></rect>
+            <rect
+              id="rect1086-6"
+              width="9.827"
+              height="9.827"
+              x="271.198"
+              y="57.641"
+              rx="0"
+              ry="0"
+            ></rect>
+          </g>
+          <path
+            id="path1109"
+            fill="none"
+            stroke="#2bff00"
+            strokeLinejoin="miter"
+            strokeWidth="7.938"
+            d="M241.338 32.128h31.372"
+          ></path>
+          <path
+            id="path1109-0"
+            fill="none"
+            stroke="#2bff00"
+            strokeLinejoin="miter"
+            strokeWidth="7.938"
+            d="M241.338 42.144h40.065"
+          ></path>
+          <path
+            id="path1109-6"
+            fill="none"
+            stroke="#2bff00"
+            strokeLinejoin="miter"
+            strokeWidth="7.938"
+            d="M243.795 51.594h25.135"
+          ></path>
+        </g>
+        <g id="baul">
+          <g
+            id="g1202"
+            fillOpacity="1"
+            stroke="none"
+            strokeDasharray="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeMiterlimit="4"
+            strokeOpacity="1"
+            strokeWidth="7.938"
+            transform="translate(-46.869 215.006)"
+          >
+            <path
+              id="rect1191"
+              fill="#5e5e5e"
+              d="M195.909 -105.304H265.666V-53.988H195.909z"
+            ></path>
+            <path
+              id="rect1193"
+              fill="#1b2519"
+              d="M195.909 -97.019H265.666V-86.328H195.909z"
+            ></path>
+            <path
+              id="rect1197"
+              fill="#fff000"
+              d="M224.239 -92.876H237.335V-79.78H224.239z"
+            ></path>
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+
+
+    </div>)
+    
+}
+
+export default Zone1;
